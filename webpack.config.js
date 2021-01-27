e8a250881e640c094ba0b9927f5d05045aa89bd8
Josiah Morris <josiahbmorris@hotmail.com>
2021-01-27 23:36:09 +1100

    Added custom fonts through webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
                 test: /\.(jpg)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            },
         ],
     },
 };

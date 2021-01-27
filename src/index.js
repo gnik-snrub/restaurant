d2e5d455fd1c15a84e3ebc43f5c35d61ea95d744
Josiah Morris <josiahbmorris@hotmail.com>
2021-01-28 00:32:57 +1100

    Added function to generate the header and navbar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import './style.css';
 
-console.log("test")
+function loadPage() {
+    const body = document.querySelector('body')
+    const header = document.createElement('header')
+
+    const headerWrapper = document.createElement('div')
+    headerWrapper.id = 'header-content-wrapper'
+
+    const headerTitle = document.createElement('p')
+    headerTitle.id = 'header-title'
+    headerTitle.textContent = "Billy Bob's Burger Bar"
+    headerWrapper.appendChild(headerTitle)
+
+    const navbar = document.createElement('nav')
+    navbar.id = 'tabs'
+
+    const homeTab = document.createElement('a')
+    homeTab.id = 'home'
+    homeTab.textContent = 'Home'
+    homeTab.href = '#'
+    navbar.appendChild(homeTab)
+
+    const menuTab = document.createElement('a')
+    menuTab.id = 'menu'
+    menuTab.textContent = 'Menu'
+    menuTab.href = '#'
+    navbar.appendChild(menuTab)
+    
+    const contactTab = document.createElement('a')
+    contactTab.id = 'contact'
+    contactTab.textContent = 'Contact Us'
+    contactTab.href = '#'
+    navbar.appendChild(contactTab)
+    headerWrapper.appendChild(navbar)
+
+    header.appendChild(headerWrapper)
+    body.appendChild(header)
+}
+
+loadPage();
